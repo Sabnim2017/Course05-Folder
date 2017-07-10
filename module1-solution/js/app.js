cb7309cf1bdfb3501ed5f94d56f6815f1ecaf7b3
Sabine <sabnimtz@outlook.com>
2017-07-09 22:38:57 -0400

    Prepare for review

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -7,8 +7,39 @@ angular.module('LunchCheck', [])
 LunchCheckController.$inject = ['$scope'];
 
 function LunchCheckController($scope) {
-	
+	$scope.lunchItems = "";
+	$scope.message = "";
+	$scope.messageColor = {};
 
-}
+  $scope.calculateMessage = function () {
+  	var items =  $scope.lunchItems.split(",");
+  	var itemsClean = items.filter(Boolean);
+  	var itemsCounter = itemsClean.length;
 
-});
+  	if (items == ""){
+  		$scope.message = "Please enter data first.";
+  		$scope.messageColor = {
+  			"color" : "red",
+  			"border" : "1px solid green",
+  			"padding" : "5px",
+  		}
+ 	} else if (itemsCounter < 4) {
+  		$scope.message = "Enjoy!";
+  		$scope.messageColor = {
+  			"color" : "green",
+  			"border" : "1px solid red",
+  			"padding" : "5px"
+  		}
+ 	} else 
+ 		$scope.message = "Too much!";
+ 		$scope.messageGreen = {
+  			"color" : "green",
+  			"border" : "1px solid red",
+  			"padding" : "5px"
+  		}
+  	}
+
+  	return $scope.message;
+  };
+
+})();
