4216807374457c346c3ee77e5e9d82712d3fc39c
Sabine <sabnimtz@outlook.com>
2017-07-23 17:40:48 -0400

    list all menu_items with ddo

--- module3-solution/js/app_test.js
+++ module3-solution/js/app_test.js
@@ -4,32 +4,31 @@
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
-.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+.directive('foundItems', FoundItems);
 
+function FoundItems() {
+  var ddo = {
+    templateUrl: 'foundItems.html'
+  };
+
+  return ddo;
+}
 
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
   var menu = this;
 
-  var promise = MenuSearchService.getMenuCategories();
+  //var menu.searchTerm ="";
+
+  var promise = MenuSearchService.getMatchedMenuItems();
 
   promise.then(function (response) {
-    menu.search = response.data;
+    menu.search = response.data.menu_items;
   })
   .catch(function (error) {
     console.log("Something went terribly wrong.");
   });
-
-  menu.logMenuItems = function (shortName) {
-    var promise = MenuSearchService.getMenuForCategory(shortName);
-
-    promise.then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-  };
 
 }
 
@@ -38,23 +37,11 @@ MenuSearchService.$inject = ['$http', 'ApiBasePath'];
 function MenuSearchService($http, ApiBasePath) {
   var service = this;
 
-  service.getMenuCategories = function () {
+  service.getMatchedMenuItems = function () {
+
     var response = $http({
       method: "GET",
-      url: (ApiBasePath + "/categories.json")
-    });
-
-    return response;
-  };
-
-
-  service.getMenuForCategory = function (shortName) {
-    var response = $http({
-      method: "GET",
-      url: (ApiBasePath + "/menu_items.json"),
-      params: {
-        category: shortName
-      }
+      url: (ApiBasePath + "/menu_items.json")
     });
 
     return response;
